64077afd40e7bb44f1d2d0cb8b3e9a97b89e454b
Edward Alvarado <edward@cotalker.com>
2020-09-30 11:00:45 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 // THESE ARE SAMPLE IMPORTS
-import { me } from './handlers/me';
-import { web } from  './handlers/web';
-import { process } from './handlers/process';
+// import { me } from './handlers/me';
+// import { web } from  './handlers/web';
+// import { process } from './handlers/process';
+
+import { handler } from './handlers/index';
 
 export function init(app) {
+    app.all('/', 'handler');
+    
     // Edit with your own routes
-    app.all('/web', web);
-    app.post('/process', process);
-    app.all('/me', me);
+    // app.all('/web', web);
+    //app.post('/process', process);
+    //app.all('/me', me);
 }
